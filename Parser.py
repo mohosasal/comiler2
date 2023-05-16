2f4735e198e5ab8366240f8cad5ab6dc000c432b
salim <mohosasal@gmail.com>
2023-05-16 19:02:22 +0330

    rewrite dfa transitions

--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@
 
 import Scanner
 from anytree import Node, RenderTree
-import transitiondiagrams
+import td
 
 
 
@@ -26,46 +26,59 @@ class Parser:
 
     def diagram_transition(self,this_node,state,line):
 
+        rented1 = False
+        rented2= False
 
 
 
+        ##########the state of finals
 
-        ############################################### final state handling
-        # if we are in the final state of the program
-
-
-        if len(transitiondiagrams.transition_diagrams[line][state]) == 0:
+        if len(td.transition_diagrams[line][state])==0:
             return
 
+        ##########other states
 
 
+        for transition in td.transition_diagrams[line][state].keys():
 
-        ###########################################first follow   (change the functionality of first follow set up)
+            #checking for transtitions first without epsilon
+            if self.token in td.first[transition] :
+                rented1=True
+                that_node = Node(transition, parent=this_node)
 
-        rented=False
-        for trans in transitiondiagrams.transition_diagrams[line][state].keys():
-            if self.token in transitiondiagrams.first[trans]:
-                rented=True
-                # add node to the tree in the left most child of the parent
-                that_node=Node(trans,parent=this_node)
-                if trans in transitiondiagrams.non_terminals:
-                    self.diagram_transition(that_node,transitiondiagrams.starter_of_non_terminals[trans],trans)
-                    #getting next token?
-                    self.diagram_transition(this_node,transitiondiagrams.transition_diagrams[line][state][self.token],line)
+                if transition in td.non_terminals:
+
+                    self.diagram_transition(that_node,td.starter_of_non_terminals[transition],transition)
+                    self.diagram_transition(this_node,td.transition_diagrams[line][state][self.token],line)
+
                 else:
                     self.token = self.scanner.get_next_token()
 
                     return
 
 
+        if rented1==False:           # epsilon handling
 
-                # where is the epsilon transition?!
+            for transition in td.transition_diagrams[line][state].keys():
+
+                if "epsilon" in td.first[transition] and self.token in td.follow[transition]:
+
+                    rented2=True
+
+                    that_node = Node(transition, parent=this_node)
+
+                    if transition in td.non_terminals:
+
+                        self.diagram_transition(that_node, td.starter_of_non_terminals[transition], transition)
+
+                    else:
+                        self.diagram_transition(this_node, td.transition_diagrams[line][state][self.token], line)
 
 
+        elif rented2==False:               #error handling
 
-        if rented==False:  # error handling
 
-            if self.token not in transitiondiagrams.follow[line]:
+            if self.token not in td.follow[line]:
 
                 #illegal token message
 
@@ -75,7 +88,7 @@ class Parser:
                 #start over from this line
 
                 self.token = self.scanner.get_next_token()
-                self.diagram_transition(this_node, transitiondiagrams.starter_of_non_terminals[line],line)
+                self.diagram_transition(this_node, td.starter_of_non_terminals[line],line)
 
             else:
 
@@ -83,12 +96,9 @@ class Parser:
 
                 error_message="missing"+" "+line
                 self.messages.append(error_message)
+                return
 
                 #exit this line
-
-                #fill this gap!
-
-
 
 
 
