6101f3a7790f4b65f50a2f449610b436116b7ca2
salim <mohosasal@gmail.com>
2023-05-18 16:05:44 +0330

    yoooo

--- My_parser.py
+++ My_parser.py
@@ -16,6 +16,7 @@ class MyParser:
         self.all_token = self.scanner.get_next_token()
         self.token = self.get_what_we_need_from_token()
         self.messages = []
+        self.EOF=False
 
     def get_what_we_need_from_token(self):
         if self.all_token[1][0] == 'NUM' or self.all_token[1][0] == 'ID':
@@ -29,7 +30,7 @@ class MyParser:
 
         ##########the state of finals
 
-        if len(td.transition_diagrams[line][state]) == 0:
+        if len(td.transition_diagrams[line][state]) == 0 or self.EOF==True:
             return
 
         ##########other states
@@ -44,7 +45,9 @@ class MyParser:
                 if transition in td.non_terminals:
                     that_node = Node(transition, parent=this_node)
                     self.diagram_transition(that_node, td.starter_of_non_terminals[transition], transition)
+                    if self.EOF == True: return
                     self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                    if self.EOF == True: return
 
                 else:
                     if self.token == '$':
@@ -54,27 +57,41 @@ class MyParser:
                     self.all_token = self.scanner.get_next_token()
                     self.token = self.get_what_we_need_from_token()
                     self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                    if self.EOF == True: return
                 break
-
+        x=[]
         if rented1 == False:  # epsilon handling
-            
-            for transition in td.transition_diagrams[line][state].keys():
 
-                if "epsilon" in td.first(transition):
+            x=td.transition_diagrams[line][state].keys()
+            
+            for transition in x :
+
+                if "epsilon" in td.first(transition) and self.token in td.follow[line]:
                     rented2 = True
                     that_node = Node(transition, parent=this_node)
 
                     if transition in td.non_terminals:
 
                         self.diagram_transition(that_node, td.starter_of_non_terminals[transition], transition)
+                        if self.EOF==True : return
                         self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                        if self.EOF==True : return
 
                     else:
                         self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                        if self.EOF == True: return
                     break
 
 
         if rented2 == False:  # error handling
+
+
+            if self.token=="$":
+                error="Unexpected EOF"
+                self.messages.append(error)
+                self.EOF=True
+                return
+
             transition = list(td.transition_diagrams[line][state].keys())[0]
             non_terminal_exist = False
             for trans in td.transition_diagrams[line][state].keys():
@@ -84,8 +101,9 @@ class MyParser:
                 error_message = 'missing ' + transition
                 self.messages.append(error_message)
                 self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                if self.EOF == True: return
                 return
-            if self.token not in td.follow[line]:
+            elif self.token not in td.follow[transition]:
 
                 # illegal token message
 
@@ -97,14 +115,17 @@ class MyParser:
                 self.all_token = self.scanner.get_next_token()
                 self.token = self.get_what_we_need_from_token()
                 self.diagram_transition(this_node, state, line)
+                if self.EOF == True: return
 
             else:
 
                 # missing line
 
-                error_message = "missing" + " " + line
+                error_message = "missing" + " " + transition
                 self.messages.append(error_message)
-                return
+                self.diagram_transition(this_node, td.transition_diagrams[line][state][transition], line)
+                if self.EOF == True: return
+
 
                 # exit this line
 
